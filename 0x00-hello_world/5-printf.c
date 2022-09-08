d8de0b67b8954a464623fcd5c5938ff2f87f4c2e
Sam Hillary <s.hillary@outlook.com>
2022-09-08 21:44:57 +0100

    C program that prints exact output using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Using printf() inside main() in C
+*
+* printf - Print exact char in C
+*
+* Return: Always Return  0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
