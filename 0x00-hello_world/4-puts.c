8baa98b53da6739ccd38cf0ae78a66154c996bbe
Sam Hillary <s.hillary@outlook.com>
2022-09-08 21:31:32 +0100

    C program that prints exact output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-* Using puts() - How to use puts inside of main in C
+* main - How to use puts() inside of main() in C
 *
-* Return: 0
+* Return: Always 0 (Success)
 */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
