18f7a2be95dad41e14ec7c95bbceef28ba8903e7
Sam Hillary <s.hillary@outlook.com>
2022-09-09 21:52:05 +0100

    If statement for basic comparison

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,16 +18,10 @@ int main(void)
 	/* your code goes there */
 
 	if (n > 0)
-	{
-		printf("%d is positive");
-	}
+		printf("%d is positive", n);
 	else if (n < 0)
-	{
-		printf("%d is negative");
-	}
+		printf("%d is negative", n);
 	else
-	{
-		printf("is zero");
-	}
+		printf("%d is zero", n);
 	return (0);
 }
