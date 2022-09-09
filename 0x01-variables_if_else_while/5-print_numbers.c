f38530f9ce6b48a3991d32fa8b264324ee93e6b5
Sam Hillary <s.hillary@outlook.com>
2022-09-10 00:42:12 +0100

    Print numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*
+* Return: Always 0 (success)
+*
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		printf("%d\n", num);
+	return (0);
+}
