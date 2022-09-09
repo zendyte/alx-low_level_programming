4e3aecedacedcc97e6c18790ad106d86417fddbf
Sam Hillary <s.hillary@outlook.com>
2022-09-09 18:33:42 +0100

    C program that prints exact output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Output when main() is call in C
+*
+* Write - Writing to std_o in C
+*
+* Return: Always Return  1 (Success)
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
