346a1cbd4f797d04a1c3aae059aa9f40cfed0554
Sam Hillary <s.hillary@outlook.com>
2022-09-10 00:17:08 +0100

    Print lower+upper case in program

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*
+* Return: Always 0 (success)
+*
+*/
+
+int main(void)
+{
+	char abc;
+	char ABC;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		putchar(abc);
+	}
+	for (ABC = 'A'; ABC <= 'Z'; ABC++)
+	{
+		putchar(ABC)
+	}
+	putchar('\n');
+	return (0);
+}
