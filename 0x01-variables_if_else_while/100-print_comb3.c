4143c1ba51fbc84db0eb4764b0aad34f7331a756
Sam Hillary <s.hillary@outlook.com>
2022-09-10 02:46:25 +0100

    Printting comb along with if statement

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*
+* Return: Always 0 (success)
+*
+*/
+
+int main(void)
+{
+	int n = 1;
+	int d1;
+	int d2;
+
+	for (d1 = 0; d1 <= 9; d1++)
+	{
+		for (d2 = n; d2 <= 9; d2++)
+		{
+			putchar(d1 + '0');
+			putchar(d2 + '0');
+			if (d1 == 9 && d2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+		n++
+	}
+	putchar('\n');
+	return (0);
+}
