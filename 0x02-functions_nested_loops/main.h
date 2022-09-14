8dd2559b64574c7afd33cb5af2472907f57aa7d0
Sam Hillary <s.hillary@outlook.com>
2022-09-14 04:34:58 +0100

    Function Declaration in C

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/**
+* _putchar - Functions Declaration
+* @c: argument
+* Return: @c on Success else -1
+*/
+
+char _putchar (char c); /* @_putchar prototype */
+
+#endif /* Check if header file _MAIN_H_ already exit */
