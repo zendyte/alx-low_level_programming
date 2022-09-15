85796d8cfd1ab6f5c47bdf8d4ffb8096336b64b4
Sam Hillary <s.hillary@outlook.com>
2022-09-16 00:55:16 +0100

    Declaration: largest_number()

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+/* positive_or_negative - check if @i is <=> 0 */
 void positive_or_negative(int i);
 
+/* largest_number - check if (@a,@b,@c) is largest number */
+int largest_number(int a, int b, int c);
 #endif /* MAIN_H */
