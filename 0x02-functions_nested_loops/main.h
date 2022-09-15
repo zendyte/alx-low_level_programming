82efb5c86aba8fe395d4384a6c8a1e6219f11148
Sam Hillary <s.hillary@outlook.com>
2022-09-15 17:13:53 +0100

    Declaration: _islowerCase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,11 +4,11 @@
 /**
 * _putchar - Functions Declaration
 * @c: argument
-* Return: @c on Success else -1
+* Return: (@c) on Success else (void)
 */
 
 char _putchar (char c); /* @_putchar prototype */
-void print_alphabet(void); /* Use @_putchar to @print_alphabet */
-void print_alphabet_x10(void); /* Use @_putchar to @print_alphabet_x10 */
-
+void print_alphabet(void); /* @_print_alphabet */
+void print_alphabet_x10(void); /* @_print_alphabet_x10 */
+int _islower(int c); /* @_islower */
 #endif /* Check if header file _MAIN_H_ already exit */
