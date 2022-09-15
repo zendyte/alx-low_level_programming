65c4d1c4667824b8d14503f962c133845de76a29
Sam Hillary <s.hillary@outlook.com>
2022-09-15 17:35:02 +0100

    Declaration: _isalpha

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,13 +2,19 @@
 #define MAIN_H_
 
 /**
-* _putchar - Functions Declaration
+* main - Functions Declaration
 * @c: argument
 * Return: (@c) on Success else (void)
 */
 
-char _putchar (char c); /* @_putchar prototype */
-void print_alphabet(void); /* @_print_alphabet */
-void print_alphabet_x10(void); /* @_print_alphabet_x10 */
-int _islower(int c); /* @_islower */
+/* _putchar - write @c to std=o & return @c on success */
+char _putchar (char c);
+/* print_alphabet - print @c & return void */
+void print_alphabet(void);
+/* print_alphabet_x10 - print @c_x10 & return void */
+void print_alphabet_x10(void);
+/* _islower - check if @c is lowercase, return 1 if true 0 otherwise */
+int _islower(int c);
+/* _isalpha - check if @c is [A-Z][a-z], return 1 if true 0 otherwise  */
+int _isalpha(int c);
 #endif /* Check if header file _MAIN_H_ already exit */
