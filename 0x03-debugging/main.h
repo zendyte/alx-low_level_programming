e263fc1dc26806b976b7f4d65ee1e9396748bda2
Sam Hillary <s.hillary@outlook.com>
2022-09-16 02:11:15 +0100

    Declaration: print_remaining_days()

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,4 +7,15 @@ void positive_or_negative(int i);
 
 /* largest_number - check if (@a,@b,@c) is largest number */
 int largest_number(int a, int b, int c);
+
+/* convert_day - converts day of month to day of year, without accounting
+* for leap year
+*/
+int convert_day(int month, int day);
+
+/* print_remaining_days - take date & print nums of days left
+* taking leap year into account 
+*/
+void print_remaining_days(int month, int day, int year);
+
 #endif /* MAIN_H */
