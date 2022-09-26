6460d2eeb825f98cdea8bca4b57396e225652eb4
Sam Hillary <s.hillary@outlook.com>
2022-09-26 22:17:02 +0100

    check if @c is sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* print_sign - check if @c is sign
+* @c: variable
+* Return: +, 1 if: (n > @c) return
+* elif: (n == @c) return 0, 0
+* fi: (n < @c) return  -, 0
+*/
+
+
+/* Function Defination - @_islower */
+
+int print_sign(int c)
+{
+	int i;
+	if (c > 0)
+	{
+		_putchar('+');
+		i = 1;
+	}
+	else if (c < 0)
+	{
+		_putchar('-');
+		i = -1;
+	}
+	else
+	{
+		_putchar('0');
+		i = 0;
+	}
+	return (i);
+}
