8489e85f34a386b87b7905ffc3bab2f278bf26da
Sam Hillary <s.hillary@outlook.com>
2022-09-26 22:35:23 +0100

    Evaluate the lenght of @s.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen - pointerFunc that evaluate the lenght of @s.
+ * @s: string param to be evaluated.
+ *
+ * Return: @len (onSuccess)
+ */
+
+int _strlen(char *s)
+{
+    int len = 0;
+
+    while (s[len] != '\0')
+    {
+        len++;
+    }
+    return len;
+}
