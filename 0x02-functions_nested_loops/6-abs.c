b70bcb3d7d2ac763553a995397e788e74be0b9cd
Sam Hillary <s.hillary@outlook.com>
2022-09-26 22:21:43 +0100

    Determine the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _abs - determine the absolute value of an integer
+* @i: variable to determine the absolute value of an integer
+* Return: i if succesful
+*/
+
+int _abs(int i)
+{
+	if (i < 0)
+	i = i * -1;
+	return (i);
+}
