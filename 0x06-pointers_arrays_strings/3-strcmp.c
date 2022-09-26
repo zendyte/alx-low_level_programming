24a3ed4feab9649de3845c1ea2bef56da0f080cb
Sam Hillary <s.hillary@outlook.com>
2022-09-26 21:50:21 +0100

    compoare two strings @s1 && @s2

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+/**
+ * _strcmp - pointerFunc that compoares two strings @s1 && @s2
+ * @s1: first string
+ * @s2: second string
+ * @ref: man strcmp
+ *
+ * Return: c1 - c2
+ */
+int _strcmp(char *s1, char *s2)
+{
+    unsigned char *p1 = (unsigned char *)s1;
+    unsigned char *p2 = (unsigned char *)s2;
+    unsigned char c1, c2;
+    do
+    {
+        c1 = *p1++;
+        c2 = *p2++;
+        if (c1 == '\0')
+            return c1 - c2;
+    } while (c1 == c2);
+    return c1 - c2;
+}
