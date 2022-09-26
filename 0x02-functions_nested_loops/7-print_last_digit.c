b0f1d3507cffd3b032b64d3f2c53bc5af96b8053
Sam Hillary <s.hillary@outlook.com>
2022-09-26 22:22:05 +0100

    Extract last number of a variable

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - extract last number of a variable
+ * @i: variable where we extract last number
+ * Return: i if succesful
+ */
+
+int print_last_digit(int c)
+{
+	c = c % 10;
+	if (c < 0)
+	c = c * -1;
+	c = c + 48;
+	_putchar(c);
+	return (c - '0');
+}
