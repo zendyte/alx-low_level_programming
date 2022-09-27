f80fe5dbdc87eac8bd86bbe39cf7c2cb6231affd
Sam Hillary <s.hillary@outlook.com>
2022-09-27 01:33:18 +0100

    Evaluate the lenght of @s.

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,14 @@
 /**
- * swap_int - Derefencing @a and @b to update its value
- * @a: First param to be swap
- * @b: Second param to be swap
- *
- *
- */
+* swap_int - Derefencing @a and @b to update its value
+* @a: First param to be swap
+* @b: Second param to be swap
+*
+*
+*/
 void swap_int(int *a, int *b)
 {
-    int *i = a;
-    int *k = b;
-    *i = 42;
-    *k = 98;
+	int *i = a;
+	int *k = b;
+	*i = 42;
+	*k = 98;
 }
