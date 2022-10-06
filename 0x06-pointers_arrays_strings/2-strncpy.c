ed8a19fa9b41abcc6237d74c16671a1a3cd201ec
Sam Hillary <s.hillary@outlook.com>
2022-10-06 14:02:41 +0100

    _strncpy - pointerFunc that copies @src to @dest including the '\0'

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,18 +3,18 @@
  * @src: soucre
  * @dest: destination
  * @n: array size/lenght to be pass
- * @ref: man strncpy
+ * ref: man strncpy
  *
  * Return: dest (onSuccess)
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++)
-        dest[i] = src[i];
-    for (; i < n; i++)
-        dest[i] = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
 
-    return dest;
+	return (dest);
 }
