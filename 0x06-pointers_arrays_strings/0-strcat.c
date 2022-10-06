7db0cc77e89451328b3d4d3969bd99810bd65b60
Sam Hillary <s.hillary@outlook.com>
2022-10-06 13:32:24 +0100

    pointerFunc that concatenates @src && @dest overwriting the '\0'.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,34 +1,37 @@
 /**
- * _strcat - pointerFunc that concatenates @src && @dest overwriting the '\0'.
- * @src: soucre
- * @dest: destination
- * @ref: man strcat
- *
- * Return: @dest (onSuccess)
- */
-
-int _strlen(char *s);
-
-char *_strcat(char *dest, char *src)
-{
-    int i, n, dest_len;
-    n = _strlen(src);
-    dest_len = _strlen(dest);
-
-    for (i = 0; i < n && src[i] != '\0'; i++)
-        dest[dest_len + i] = src[i];
-    dest[dest_len + i] = '\0';
-
-    return dest;
-}
+* _strlen - pointerFunc that evaluate the lenght of @s
+* @s: string param to be evaluated.
+*
+* Return: @len (onSuccess)
+*/
 
 int _strlen(char *s)
 {
-    int len = 0;
+	int len = 0;
 
-    while (s[len] != '\0')
-    {
-        len++;
-    }
-    return len;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+* _strcat - pointerFunc that concatenates @src && @dest overwriting the '\0'.
+* @src: soucre
+* @dest: destination
+* ref: man strcat
+*
+* Return: @dest (onSuccess)
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i, n, dest_len;
+
+	n = _strlen(src);
+	dest_len = _strlen(dest);
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+	return (dest);
 }
