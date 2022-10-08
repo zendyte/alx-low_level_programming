690cf5ebbd6738983ad514e8ef7b7f680c3c5b57
Sam Hillary <s.hillary@outlook.com>
2022-10-08 16:35:44 +0100

    FuncPrototype: string_toupper

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* _strcat - pointerFunc that concatenates @src && @dest overwirting the '\0'. */
+/* _strcat - concatenates @src && @dest overwirting the '\0'. */
 char *_strcat(char *dest, char *src);
 
-/* _strncat - pointerFunc that concatenates @src && @dest overwriting the '\0' */
+/* _strncat - concatenates @src && @dest overwriting the '\0' */
 char *_strncat(char *dest, char *src, int n);
 
 /* _strncpy - pointerFunc that copies @src to @dest including the '\0' */
@@ -13,6 +13,10 @@ char *_strncpy(char *dest, char *src, int n);
 /* _strcmp - pointerFunc that compoares two strings @s1 && @s2 */
 int _strcmp(char *s1, char *s2);
 
-/* reverse_array - pointerFunc that reverses the content of an array @a of integers.*/
+/* reverse_array - reverses the content of an array @a of integers.*/
 void reverse_array(int *a, int n);
+
+/* string_toupper - changes all lowercase letters of a @str to uppercase */
+char *string_toupper(char *str);
+
 #endif /* MAIN_H */
